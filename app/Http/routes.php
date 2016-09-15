7e18b15b9b44841ab7213e2c08b54df6ea8e92b6
M Arfan <arfan67@gmail.com>
2016-09-15 06:46:08 +0000

    Applied fixes from StyleCI

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,53 +29,46 @@ Route::post('/send_request', 'HomeController@send_request');
 Route::get('/contact-us', 'HomeController@contact');
 Route::get('/about-us', 'HomeController@about');
 Route::get('/loan-calculator', 'HomeController@calculator');
-Route::get('localization/{locale}','LocalizationController@index');
+Route::get('localization/{locale}', 'LocalizationController@index');
 Route::auth();
 
 Route::group(['middleware' => 'web'], function () {
+    Route::get('/admin', 'DashboardController@index');
+    Route::get('/dashboard', 'DashboardController@index');
+    Route::get('/customer-requests', 'DashboardController@customer_requets');
+    Route::get('/profile', 'HomeController@profile');
+    Route::get('/categories', 'CategoryController@index');
+    Route::post('/category/save', 'CategoryController@save');
+    Route::post('/category/get', 'CategoryController@get');
+    Route::post('/category/delete', 'CategoryController@delete');
 
+    Route::get('/agents', 'AgentsController@index');
+    Route::post('/agents/save', 'AgentsController@save');
+    Route::post('/agents/get', 'AgentsController@get');
+    Route::post('/agents/delete', 'AgentsController@delete');
+    Route::get('/verify_email', 'AgentsController@verify_email');
+    Route::get('register/verify/{confirmationCode}', 'AgentsController@confirm');
+    Route::post('agents/update_profile', 'AgentsController@update_profile');
+    Route::post('agents/password_reset/', 'AgentsController@password_reset');
 
+    Route::get('/features', 'FeatureController@index');
+    Route::post('/feature/save', 'FeatureController@save');
+    Route::post('/feature/get', 'FeatureController@get');
+    Route::post('/feature/delete', 'FeatureController@delete');
 
-Route::get('/admin', 'DashboardController@index');
-Route::get('/dashboard', 'DashboardController@index');
-Route::get('/customer-requests', 'DashboardController@customer_requets');
-Route::get('/profile', 'HomeController@profile');
-Route::get('/categories', 'CategoryController@index');
-Route::post('/category/save', 'CategoryController@save');
-Route::post('/category/get', 'CategoryController@get');
-Route::post('/category/delete', 'CategoryController@delete');
+    Route::post('/property/fileupload', 'PropertyController@fileupload');
+    Route::get('/properties', 'PropertyController@index');
+    Route::get('/property/add', 'PropertyController@form');
+    Route::get('/listing/edit/{id}', 'PropertyController@edit');
+    Route::get('/listing/image_delete/{id}', 'PropertyController@image_delete');
+    Route::post('/property/save', 'PropertyController@save');
+    Route::post('property/addTofeatured', 'PropertyController@addTofeatured');
+    Route::post('property/addToArchive', 'PropertyController@addToArchive');
 
-Route::get('/agents', 'AgentsController@index');
-Route::post('/agents/save', 'AgentsController@save');
-Route::post('/agents/get', 'AgentsController@get');
-Route::post('/agents/delete', 'AgentsController@delete');
-Route::get('/verify_email', 'AgentsController@verify_email');
-Route::get('register/verify/{confirmationCode}' , 'AgentsController@confirm');
-Route::post('agents/update_profile', 'AgentsController@update_profile');
-Route::post('agents/password_reset/', 'AgentsController@password_reset');
-
-Route::get('/features', 'FeatureController@index');
-Route::post('/feature/save', 'FeatureController@save');
-Route::post('/feature/get', 'FeatureController@get');
-Route::post('/feature/delete', 'FeatureController@delete');
-
-Route::post('/property/fileupload', 'PropertyController@fileupload');
-Route::get('/properties', 'PropertyController@index');
-Route::get('/property/add', 'PropertyController@form');
-Route::get('/listing/edit/{id}', 'PropertyController@edit');
-Route::get('/listing/image_delete/{id}', 'PropertyController@image_delete');
-Route::post('/property/save', 'PropertyController@save');
-Route::post('property/addTofeatured', 'PropertyController@addTofeatured');
-Route::post('property/addToArchive', 'PropertyController@addToArchive');
-
-Route::get('/settings', 'SettingController@index');
-Route::post('/setting/save', 'SettingController@save');
+    Route::get('/settings', 'SettingController@index');
+    Route::post('/setting/save', 'SettingController@save');
 });
 
-Route::group(['prefix' => 'api/v1'], function() {
-	Route::get('properties', 'ApiController@index');	
-	
-	
-	
-	
+Route::group(['prefix' => 'api/v1'], function () {
+    Route::get('properties', 'ApiController@index');
 });
