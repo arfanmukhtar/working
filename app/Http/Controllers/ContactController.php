6c7e9c5e770f53c7502e671015055505be9ae916
Arfan Ali <arfan67@gmail.com>
2016-09-15 11:37:15 +0500

    error fix

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -19,7 +19,7 @@ class ContactController extends Controller
     public function index()
     {	
 		if(Auth::user()->AccessLevel == 1) { 
-			$contacts = Category::->orderBy("id" , "desc")->paginate(25);
+			$contacts = Category::orderBy("id" , "desc")->paginate(25);
 		} else { 
 			$contacts = Category::where("agent_id" , Auth::user()->id)->orderBy("id" , "desc")->paginate(25);
 		}
